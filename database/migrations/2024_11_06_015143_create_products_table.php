38f491a6873ee46703cb00484da4fa9852b737f6
ricomuh <rico.smartlolo@gmail.com>
2025-01-07 17:52:51 +0700

    Add image_url field to products table and implement ProductSeeder for initial data

--- database/migrations/2024_11_06_015143_create_products_table.php
+++ database/migrations/2024_11_06_015143_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('image_url');
             $table->string('name');
             $table->text('description');
             $table->decimal('price', 8, 2);
